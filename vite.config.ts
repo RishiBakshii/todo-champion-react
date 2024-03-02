b57356a2f8112a2b7ca884798a85872548049185
rishibakshi <guitarguyrishi@gmail.com>
2024-03-02 16:20:53 +0530

    feat(Pwa): Add pwa optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Todo Champion",
+        short_name: "Todo Champ",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "maskable.png",
+            sizes: "196x196",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "logo384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
